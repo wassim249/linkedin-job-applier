2eab040c605ccde4be06768f3c64d293ebf1bebc
Wassim EL BAKKOURI <62564480+Wassim249@users.noreply.github.com>
2022-07-09 13:19:30 +0100

    add filter jobs feature

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,71 @@ const start = async (): Promise<void> => {
     console.log("Login failed");
     return;
   }
+
+  // generate jobs page link
+  const jobsPageLink = (): string => {
+    let link: string = "https://www.linkedin.com/jobs/search/?f_AL=true";
+    // load arguments from .env file
+    const LOCATION: string | undefined = process.env.LOCATION;
+    const POSITION: string | undefined = process.env.POSITION;
+    let expericences: string[] = process.env.EXPERIENCES
+      ? process.env.EXPERIENCES.split(" ")
+      : [];
+    let jobTypes: string[] = process.env.JOB_TYPES
+      ? process.env.JOB_TYPES.split(" ")
+      : [];
+    let onSiteOrRemote: string[] = process.env.ON_SITE_OR_REMOTE
+      ? process.env.ON_SITE_OR_REMOTE.split(" ")
+      : [];
+    // check if location and position are set
+    if (!LOCATION || !POSITION) {
+      console.log("Please set LOCATION, POSITION in .env file");
+      process.exit(1);
+    }
+    // array of linkedin experience levels
+    const LINKEDIN_EXPERIENCES: string[] = [
+      "Internship",
+      "Entry-level",
+      "Associate",
+      "Mid-Senior",
+      "Director",
+      "Executive",
+    ];
+
+    // array of linkedin on site or remote options
+    const LINKEDIN_ON_SITE_OR_REMOTE: string[] = [
+      "On-site",
+      "Remote",
+      "Hybrid",
+    ];
+
+    // add experiences to link
+    expericences.forEach((exp: string, index: number) => {
+      exp = `${LINKEDIN_EXPERIENCES.indexOf(exp) + 1}`;
+      link += index == 0 ? `&f_E=${exp}` : `%2C${exp}`;
+    });
+    // add job types to link
+    jobTypes.forEach((jobType: string, index: number) => {
+      jobType = jobType[0].toUpperCase();
+      link += index == 0 ? `&f_JT=${jobType}` : `%2C${jobType}`;
+    });
+
+    // add on site or remote to link
+    onSiteOrRemote.forEach((onSiteOrRemote: string, index: number) => {
+      onSiteOrRemote = `${
+        LINKEDIN_ON_SITE_OR_REMOTE.indexOf(onSiteOrRemote) + 1
+      }`;
+      link += index == 0 ? `&f_WT=${onSiteOrRemote}` : `%2C${onSiteOrRemote}`;
+    });
+
+    // add location and postion to link
+    link += `&keywords=${POSITION}&location=${LOCATION}`;
+
+    return link;
+  };
+
+  // go to jobs page
+  await tab.goto(jobsPageLink());
 };
 
 start();
